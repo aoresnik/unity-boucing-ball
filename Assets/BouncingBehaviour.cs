8c541d433a5f3f328588dfac684577289f552fb3
Andrej Orešnik <andrej7@gmail.com>
2020-01-14 02:37:14 +0100

    Fixed formatting

--- Assets/BouncingBehaviour.cs
+++ Assets/BouncingBehaviour.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class BouncingBehaviour : MonoBehaviour
 {
-	const float V0 = 7f;
-	
-	const float A = 9.81f;
-	
+    const float V0 = 7f;
+        
+    const float A = 9.81f;
+        
     void Update()
     {
-		float tOneBounce = (2 * V0) / A;
-		float t = Time.time % tOneBounce;
-		
+        float tOneBounce = (2 * V0) / A;
+        float t = Time.time % tOneBounce;
+
         transform.localPosition = new Vector3(0f,1.0f,0f) * ((V0 * t) - (0.5f * A * (t * t)));
     }
 }
